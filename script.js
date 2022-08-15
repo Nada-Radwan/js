9d83f292a2bda1a431d957e6aa4de380ef1b193a
Nada-Radwan <74054255+Nada-Radwan@users.noreply.github.com>
2022-08-15 20:38:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,172 @@
+// 1-Check The Number Is Even or Odd
+// num = prompt("PLeaser enter a number");
+// let number=parseInt(num);
+// if(number%2==0)
+// console.log("EVEN")
+// else
+// console.log("ODD")
+//
+
+//2- fizzbuzz
+// for(let i=1; i<=100; i++){
+//     if(i % 3 === 0 && i % 5){
+//            console.log("fizzBuzz")
+//        }
+//      else if (i % 3 === 0){
+//            console.log("fizz")
+//        }else if(i % 5 === 0){
+//            console.log("Buzz")
+//        }else{
+//            console.log(i)
+//        }
+//        }
+
+
+//3-Reverse String
+// var str = prompt("Please enter any word");
+// let reversed = [];
+// 	for (i = str.length - 1; i >= 0; i--) {
+// 		reversed.push(str[i]);
+// 	}
+// let x=reversed.join('');
+// console.log(x)
+
+
+//4- Compute Circle Area and Circumference
+// let pi = 3.14;
+// rad = prompt("Please enter radius:");
+// let r=parseInt(rad);
+// let area = pi*r*r;
+// let c = 2*pi*r;
+// console.log("Area of circle is "+area)
+// console.log("Circumference of circle is "+c)
+
+
+
+//5- check two given numbers and return true if one of the number is 50 or if their sum is 50.
+// num1 = prompt("Please enter first number:");
+// num2 = prompt("Please enter second number:");
+// let a=parseInt(num1);
+// let b=parseInt(num2);
+//     if( (a||b) == 50 || (a+b)==50)
+//     console.log("true");
+//     else 
+//     console.log("false");
+
+
+//6- check from two given integers, whether one is positive and another one is negative. 
+// num1 = prompt("Please enter first number:");
+// num2 = prompt("Please enter second number:");
+// let a=parseInt(num1);
+// let b=parseInt(num2);
+//     if((a<0 && b>0)||(a>0 && b<0))
+//     console.log("True Both number have different signs")
+//     else 
+//     console.log("Wrong Both have the same signs")
+
+
+//7- check whether a given positive number is a multiple of 5 or a multiple of 8.
+//  num1 = prompt("Please enter first number:");
+//  let a=parseInt(num1);
+//  if(a%5==0)
+//  console.log("The number is a multiple of 5")
+//  else if(a%8==0)
+//  console.log("The number is a multiple of 8")
+//  else 
+//  console.log("The number is neither multiple of 5 not 8")
+
+
+//8- find the largest of three given integers
+// num1 = prompt("Please enter first number:");
+// num2 = prompt("Please enter second number:");
+// num3 = prompt("Please enter third number:");
+// let a=parseInt(num1);
+// let b=parseInt(num2);
+// let c=parseInt(num3);   
+// let largest=0;
+// if(a>b&&a>c)
+// largest=a; 
+// else if(b>a&&b>c)
+// largest=b;
+// else
+// largest=c;
+
+// console.log("largest number is "+largest)
+
+
+//9- Compute The sum of the numbers from 1 to 10
+// let sum=0;
+// for(let i=1;i <=10;i++)
+// {
+// sum+=i;
+
+// }
+// console.log("the sum is "+sum)
+
+
+//10- display the pattern like right angle triangle using an asterisk. 
+// let sum="*";
+// console.log("the sum is "+sum)
+// for(let i=1;i <5;i++)
+// {
+// sum=sum+"*";
+// console.log("the sum is "+sum)
+// }
+
+//11- check whether a given number is positive or negative. 
+// num1 = prompt("Please enter first number:");
+// let a=parseInt(num1);
+//     if(a<0 )
+//     console.log("Negative")
+//     else 
+//     console.log("Positive")
+
+
+//12- array
+// const arr = [];
+// for(let i=0;i<10;i++)
+// {
+//     console.log("element - `${i},` : " )
+//     arr[i]=prompt("");
+//     console.log(arr[i])
+
+// }
+
+  
+
+
+
+//13- print the sum of two numbers
+// num1 = prompt("Please enter first number:");
+// num2 = prompt("Please enter second number:");
+// let a=parseInt(num1);
+// let b=parseInt(num2);
+  
+//     console.log(a+b)
+
+
+//14- calculate the factorial of a given numberTest Data :
+// num1 = prompt("Please enter first number:");
+// let a=parseInt(num1);
+
+// for(let i=a-1;i>0;i--)
+// {
+//     a*=i;
+// }
+// console.log(a)
+
+//15- Create Simple Calculator (+ , - , * , /) 
+//   num1 = prompt("Please enter first number:");
+//   num2 = prompt("Please enter second number:");
+//   choose=prompt("please press + or - or * or /");
+//  let x=parseInt(num1);
+//  let y=parseInt(num2);
+
+//  if(choose=='+'){ console.log(x+y)}
+//  else if(choose=='-'){ console.log(x-y)}
+//  else if(choose=='*'){ console.log(x*y)}
+//  else if(choose=='/'){ console.log(x/y)}
+//  else console.log("input you enterd is wrong")
+
+
+
